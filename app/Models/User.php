0b2c37661f84ca99d71c4a87a0844eea4ad5e16d
aartemkin <artemkin030@gmail.com>
2024-04-17 22:48:05 +0400

    Created rooms model & migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Report::class);
     }
+
+    public function rooms(): HasMany
+    {
+        return $this->hasMany(Room::class);
+    }
 }
