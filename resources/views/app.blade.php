06183d0afb464c6b56a4a71f6db6c3108923ce1c
aartemkin <artemkin030@gmail.com>
2024-05-05 19:44:02 +0400

    Fixed error with adaptive in telegram web app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,13 +2,19 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>darkcards</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
     <script src="https://unpkg.com/centrifuge@5.0.0/dist/centrifuge.js"></script>
     <style rel="/css/base.css"></style>
     <script>
         const telegram = window.Telegram.WebApp
         telegram.expand()
-        telegram.onEvent('viewportChanged', () => telegram.expand())
     </script>
     @vite(['resources/js/app.js', 'resources/css/app.scss'])
     @inertiaHead
