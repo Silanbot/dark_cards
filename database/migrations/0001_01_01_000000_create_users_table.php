c8e20424314ad682e338bdb6e663261cd077f5b5
aartemkin <artemkin030@gmail.com>
2024-05-18 02:08:53 +0400

    Updated migration && GitHub Action

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->unique();
             $table->string('username');
             $table->float('balance')->default(0);
             $table->rememberToken();
