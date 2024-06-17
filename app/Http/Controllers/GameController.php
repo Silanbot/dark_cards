e557c561cc00e1c5d9b4d7f21c27f6ff07be2439
Sequencer <45060278+sequencerr@users.noreply.github.com>
2024-06-17 14:58:29 +0200

    Fix ready_state NULL x2
    - use Collection properly instead.
    - leaving unset() in favor of in-place modification. otherwise should be `$state = $state->filter(fn ($v) => $v !== $request->user_id)`

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -45,20 +45,17 @@ class GameController extends Controller
 
     public function setReadyState(Request $request, Room $room): void
     {
-        $state = $room->ready_state;
-        if (is_null($state)) $state = [];
-        if (in_array($request->user_id, $state->toArray()))
-            unset($state[array_search($request->user_id, $state->toArray())]);
+        $state = $room->ready_state ?? collect([]);
+        if ($state->contains($request->user_id))
+            unset($state[$state->search($request->user_id)]);
         else {
-            $state[] = $request->user_id;
+            $state->add($request->user_id);
 
             if ($room->max_gamers === count($state))
                 $this->contract->allPlayersReady($room->id);
         }
 
-        $room->update([
-            'ready_state' => $state,
-        ]);
+        $room->update(['ready_state' => $state->toArray()]);
     }
 
     public function leave(Request $request): void
