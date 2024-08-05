10628c2c6668668712de549284941d453ce53dbc
artemkin <artemkin030@gmail.com>
2024-08-06 00:06:34 +0400

    Обновление создания пользователя

--- resources/js/Pages/api/telegram.js
+++ resources/js/Pages/api/telegram.js
@@ -4,22 +4,22 @@ export default new (class {
 
 	constructor() {
 		console.log('import.meta.env.VITE_DEV', import.meta.env.VITE_DEV);
-		if (import.meta.env.VITE_DEV && !window.Telegram.WebApp.initDataUnsafe.user) {
-			window.Telegram.WebApp.initDataUnsafe.user = new Promise(async r =>
-				r(
-					(await navigator.storage.estimate()).quota < window?.performance?.memory?.jsHeapSizeLimit ??
-						1073741824 * 2
-						? { id: 2, username: 'Sequencer' }
-						: { id: 1, username: 'KaptainMidnight' }
-				)
-			);
-		} else window.Telegram.WebApp.initDataUnsafe.user = Promise.resolve(window.Telegram.WebApp.initDataUnsafe.user);
-
-		this.telegram = window.Telegram.WebApp;
+		// if (import.meta.env.VITE_DEV && !window.Telegram.WebApp.initDataUnsafe.user) {
+		// 	window.Telegram.WebApp.initDataUnsafe.user = new Promise(async r =>
+		// 		r(
+		// 			(await navigator.storage.estimate()).quota < window?.performance?.memory?.jsHeapSizeLimit ??
+		// 				1073741824 * 2
+		// 				? { id: 2, username: 'Sequencer' }
+		// 				: { id: 1, username: 'KaptainMidnight' }
+		// 		)
+		// 	);
+		// } else window.Telegram.WebApp.initDataUnsafe.user = Promise.resolve(window.Telegram.WebApp.initDataUnsafe.user);
+        //
+		// this.telegram = window.Telegram.WebApp;
 		this.user = window.Telegram.WebApp.initDataUnsafe.user;
-		(async () => {
-			console.log('tg.user', await window.Telegram.WebApp.initDataUnsafe.user);
-		})();
+		// (async () => {
+		// 	console.log('tg.user', await window.Telegram.WebApp.initDataUnsafe.user);
+		// })();
 	}
 
 	notificationFeedback(style) {
