b2b4b0e371a4b516b67990850c74630992d6ebd2
artemkin <artemkin030@gmail.com>
2024-09-01 22:09:50 +0400

    Исправление подключения к игре

--- app/Http/Controllers/Game/GameController.php
+++ app/Http/Controllers/Game/GameController.php
@@ -48,7 +48,7 @@ class GameController extends Controller
 
         $alreadyJoined = $room->join_state ?? collect([]);
         if ($alreadyJoined->contains($player)) {
-            return response()->json($alreadyJoined);
+            return response()->json(User::query()->findMany($alreadyJoined));
         }
 
         $alreadyJoined->add($player);
