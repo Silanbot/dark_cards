e1f5170ac7b7e542e702d800c484604e9499d0f7
artemkin <artemkin030@gmail.com>
2024-09-01 21:47:29 +0400

    Исправление подключения к игре

--- resources/js/Pages/api/composable/useWebsocket.js
+++ resources/js/Pages/api/composable/useWebsocket.js
@@ -11,10 +11,10 @@ export default function useWebsocket(token, subscription) {
         onConnected: callback => centrifugo.on('connected', callback),
         runListening: () => {
             centrifugo.getSubscription(subscription).on('publication', ({ context }) => {
-                // const listener = listeners.find(listener => listener.event === context.event)
-                // console.log(listener)
+                const listener = listeners.find(listener => listener.event === context.event)
+                console.log(listener)
                 console.log(context.event, context)
-                // listeners.find(listener => listener.event === context.event).handler(context)
+                listeners.find(listener => listener.event === context.event).handler(context)
             })
             centrifugo.getSubscription(subscription).subscribe()
             centrifugo.connect()
