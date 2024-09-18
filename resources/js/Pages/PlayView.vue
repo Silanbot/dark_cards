bfa4b82d6e0b64d7f592c9005aaac353c849b038
artemkin <artemkin030@gmail.com>
2024-09-18 22:42:03 +0400

    Изменена логика перехода игры

--- resources/js/Pages/PlayView.vue
+++ resources/js/Pages/PlayView.vue
@@ -231,13 +231,9 @@ import modalDialog from './components/modalDialog.vue'
                     <div class="footer__button" @click="setReadyState" v-else>Не готов</div>
                 </template>
                 <template v-else>
-                    <div class="footer__button" @click="startFinishBeat"       v-if="myTurn && gameCells.some(cell => cell.length === 2)">Бито</div>
+                    <div class="footer__button" @click="startFinishBeat"       v-if="myTurn && gameCells.filter(c => c.length).length && gameCells.filter(c => c.length).every(c => c.length === 2)">Бито</div>
                     <div class="footer__button"                                v-else-if="myTurn">Ваш ход</div>
-<!--                    gameCells.every(c => !c.length)-->
-                    <div class="footer__button" @click="() => takeFromTable()" v-else :style="{
-                        visibility: !myTurn && gameCells.length > 0 && gameCells.some(cell => cell.length > 0) &&
-                                gameCells.some(cell => cell.length === 1) &&
-                                gameCells.every(cell => cell.length === 2) ? 'hidden' : undefined }">Взять</div>
+                    <div class="footer__button" @click="() => takeFromTable()" v-else :style="{ visibility: gameCells.every(c => !c.length) ? 'hidden' : undefined }">Взять</div>
                 </template>
 
                 <div class="footer__person">
