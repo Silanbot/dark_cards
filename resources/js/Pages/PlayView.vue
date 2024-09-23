bdba294693f5b50a8e5a56ca23bd5b8b11388de6
valerybondarev <valerysbondarev@gmail.com>
2024-09-24 00:46:41 +0300

    Fix кнопок Бито и Взять

--- resources/js/Pages/PlayView.vue
+++ resources/js/Pages/PlayView.vue
@@ -232,7 +232,7 @@ import modalDialog from './components/modalDialog.vue'
                 </template>
                 <template v-else>
                     <div class="footer__button" @click="startFinishBeat"       v-if="(myTurn && gameCells.filter(c => c.length).length > 0 && gameCells.filter(c => c.length).every(c => c.length % 2 === 0))">Бито</div>
-                    <div class="footer_button_turn"                                v-else-if="myTurn">Ваш ход</div>
+                    <div class="footer__button"                                v-else-if="myTurn">Ваш ход</div>
                     <div class="footer__button" @click="() => takeFromTable()" v-else :style="{ visibility: (myTurn  && gameCells.every(c => !c.length) && gameCells.filter(c => c.length).length !== 0) || (gameCells.filter(c => c.length).length === 0) || (!myTurn && (gameCells.filter(c => c.length).length+2) %2 === 1) ? 'hidden' : undefined }">Взять</div>
                 </template>
 
